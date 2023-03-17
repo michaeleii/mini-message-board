c1118ffc8b28f628255abaebb3bb013488670d55
Michael Lei <michaelleie0ca@gmail.com>
2023-03-16 18:35:28 -0700

    Create views, models, and public folders

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,4 +1,8 @@
-const index = () => {};
-const createMsg = () => {};
+const messages = require("../models/messages");
+
+const index = (req, res, next) => {
+	res.render("index", { title: "Mini Messageboard", messages });
+};
+const createMsg = (req, res, next) => {};
 
 module.exports = { index, createMsg };
